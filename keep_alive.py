1a00d8dfe87dcc56d5e94df8011422ac11454a14
RoxyRekt <87309991+roxyrekt@users.noreply.github.com>
2024-05-15 20:56:20 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,7 +3,7 @@ from threading import Thread
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Rilevium Jailbreak -- Roxy"
+    return "Rilevium Jailbreak -- Developer - Roxy"
 def run():
   app.run(host='0.0.0.0',port=8080)
 def keep_alive():  
