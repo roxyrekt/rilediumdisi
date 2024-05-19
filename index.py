28de8c1a7de355a31a740ef54eaf0690c6cdfa11
RoxyRekt <87309991+roxyrekt@users.noreply.github.com>
2024-05-19 21:08:39 +0300

    Update index.py

--- index.py
+++ index.py
@@ -27,14 +27,13 @@ api_key = steam_api_key
 @bot.listen('on_message')
 async def on_message(message):
     # Sadece belirli bir kanaldaki mesajları işle
-    if message.channel.id != 1232074719486808184:
+    if message.channel.id != 1239701061179150346:
         if message.content.startswith('!steamlink'):
-            steam_link = message.content[len('!steamlink'):].strip()
             await message.channel.send("Sadece VEGAS reisin ayarladığı kanallarda çalışır.")
             return
 
     # steamlink komutu
-    if message.content.startswith('!steamlink'):
+    if message.channel.id == 1239701061179150346 and message.content.startswith('!steamlink'):
         steam_link = message.content[len('!steamlink'):].strip()
 
         # Steam profil URL'sinden SteamID64'ü alma
@@ -56,6 +55,35 @@ async def on_message(message):
         steam_id3 = str(int(steam_id64) - 76561197960265728)
         steam_id31 = f"[U:1:{steam_id3}]"
         await message.channel.send(f"Komutu kullanan: {message.author.mention}\n<a:ok23:1231431234907799682> **SteamID3:** `{steam_id31}`\n<a:ok23:1231431234907799682> **SteamID64:** `{steam_id64}`")
+
+# steamid komutu
+@bot.command(name='steamid')
+async def steamid(ctx, steam_id: str):
+    # Sadece belirli bir kanalda çalışsın
+    if ctx.channel.id != 1239701061179150346:
+        await ctx.send("Sadece VEGAS reisin ayarladığı kanallarda çalışır.")
+        return
+    
+    try:
+        if steam_id.isdigit():
+            # SteamID64 işle
+            steam_id64 = steam_id
+            steam_id3 = str(int(steam_id64) - 76561197960265728)
+            steam_id31 = f"[U:1:{steam_id3}]"
+        elif steam_id.startswith("[U:1:") and steam_id.endswith("]"):
+            # SteamID3 işle
+            steam_id3 = steam_id[5:-1]
+            steam_id64 = str(int(steam_id3) + 76561197960265728)
+            steam_id31 = steam_id
+        else:
+            await ctx.send("Geçersiz SteamID formatı.")
+            return
+        
+        steam_profile_url = f"https://steamcommunity.com/profiles/{steam_id64}"
+
+        await ctx.send(f"Komutu kullanan: {ctx.author.mention}\n<a:ok23:1231431234907799682> **Steam Profili:** {steam_profile_url}\n<a:ok23:1231431234907799682> **SteamID3:** `{steam_id31}`\n<a:ok23:1231431234907799682> **SteamID64:** `{steam_id64}`")
+    except ValueError:
+        await ctx.send("Geçersiz SteamID.")
 
 # Botu çalıştırma
 bot.run(discord_token)
