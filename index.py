00129b45b4a37ec17a82e1bc3684765883813b7e
RoxyRekt <87309991+roxyrekt@users.noreply.github.com>
2024-06-11 01:32:58 +0300

    Update index.py

--- index.py
+++ index.py
@@ -67,8 +67,20 @@ async def steamid(ctx, steam_identifier: str):
             steam_id31 = f"[U:1:{steam_id3}]"
         
         steam_profile_url = f"https://steamcommunity.com/profiles/{steam_id64}"
+        
+        # Profil bilgilerini al
+        profile_response = requests.get(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={api_key}&steamids={steam_id64}")
+        profile_data = profile_response.json()
+        player_info = profile_data['response']['players'][0]
 
-        await ctx.send(f"Komutu kullanan: {ctx.author.mention}\n<a:ok23:1231431234907799682> **Steam Profili:** {steam_profile_url}\n<a:ok23:1231431234907799682> **SteamID3:** `{steam_id31}`\n<a:ok23:1231431234907799682> **SteamID64:** `{steam_id64}`")
+        # Ek bilgileri ayıkla
+        profile_created = player_info.get('timecreated', 'Bilinmiyor')
+        profile_state = 'Public' if player_info.get('communityvisibilitystate', 0) == 3 else 'Private'
+        profile_name = player_info.get('personaname', 'Bilinmiyor')
+        profile_location = player_info.get('loccountrycode', 'Bilinmiyor')
+        profile_custom_url = player_info.get('profileurl', 'Bilinmiyor')
+        
+        await ctx.send(f"Komutu kullanan: {ctx.author.mention}\n<a:ok23:1231431234907799682> **Steam Profili:** {steam_profile_url}\n<a:ok23:1231431234907799682> **SteamID3:** `{steam_id31}`\n<a:ok23:1231431234907799682> **SteamID64:** `{steam_id64}`\n<a:ok23:1231431234907799682> **Profil Durumu:** {profile_state}\n<a:ok23:1231431234907799682> **Profil Oluşturulma Tarihi:** {profile_created}\n<a:ok23:1231431234907799682> **Kullanıcı Adı:** {profile_name}\n<a:ok23:1231431234907799682> **Konum:** {profile_location}\n<a:ok23:1231431234907799682> **Özel URL:** {profile_custom_url}")
     except ValueError:
         await ctx.send("Geçersiz SteamID.")
 
