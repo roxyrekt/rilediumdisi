f3673199bca5fb39296f27a2685b84b06beb726c
RoxyRekt <87309991+roxyrekt@users.noreply.github.com>
2024-06-11 17:03:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,18 @@ discord_token = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Komutları yükle
-bot.load_extension("commands.steamid")
+async def load_extensions():
+    await bot.load_extension("commands.steamid")
+
+@bot.event
+async def on_ready():
+    await load_extensions()
+    await bot.tree.sync()  # Slash komutlarını senkronize et
+    print(f"Logged in as {bot.user}")
 
 keep_alive()
 
